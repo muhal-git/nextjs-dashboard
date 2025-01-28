abe8e517c6b2919d45807fa62f0484023a67ea31
Muhammet İRİŞ <73129339+muhal-git@users.noreply.github.com>
2025-01-28 12:50:47 +0000

    Add Vercel Analytics integration and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import '@/app/ui/global.css';
 
 import { inter } from '@/app/ui/fonts';
 
+import { Analytics } from "@vercel/analytics/react"
+
 export default function RootLayout({
   children,
 }: {
